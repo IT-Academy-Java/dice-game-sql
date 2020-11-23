8feecd1d16b22f4e6bfdcfe126ea70f923d6f5a2
Elena Piaggio <elenapiaggio@gmail.com>
2020-11-23 16:37:23 +0100

    add try catch

--- src/main/java/com/itacademy/dicesgame/controller/GameController.java
+++ src/main/java/com/itacademy/dicesgame/controller/GameController.java
@@ -1,8 +1,11 @@
 package com.itacademy.dicesgame.controller;
 
 import com.itacademy.dicesgame.entity.Game;
+import com.itacademy.dicesgame.exception.PlayerNotFoundException;
 import com.itacademy.dicesgame.service.impl.GameServiceImpl;
+import com.itacademy.dicesgame.service.impl.PlayerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.util.BsonUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,34 +18,45 @@ import java.util.List;
 public class GameController {
 
     @Autowired
-    private GameServiceImpl service;
+    private GameServiceImpl gameService;
+
+    @Autowired
+    private PlayerServiceImpl playerService;
 
     @GetMapping("/{id}/games")
     @ResponseBody
     public List<Game> getGamesByPlayer(@PathVariable(value = "id") Long id) throws Exception{
-        if(id != null){
-            try {
-                return service.getGamesByPlayer(id);
-            } catch (Exception e){
-                System.out.println(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getCause()));
+        try {
+            if(id != null && gameService.getGamesByPlayer(id) != null){
+                return gameService.getGamesByPlayer(id);
+            } else{
+                throw new PlayerNotFoundException("Player with id -> " + id + " does not exist!!!");
             }
+        } catch (Exception e){
+            System.out.println("Error -> " + e.getMessage());
         }
         return null;
     }
 
     @PostMapping("/{id}/games")
     public ResponseEntity<Game> addRollDices(@PathVariable(value="id") Long id) throws Exception{
-        return ResponseEntity.ok().body(service.rollDices(id));
+        return ResponseEntity.ok().body(gameService.rollDices(id));
     }
 
     @DeleteMapping("/{id}/games")
     public ResponseEntity deleteRollsByPlayer(@PathVariable(value = "id") Long id) throws Exception{
          try {
-             service.deleteByPlayerId(id);
-             return ResponseEntity.ok().build();
-         } catch (Exception exception){
-             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getCause());
+             if(id != null && playerService.findPlayer(id) != null){
+                 gameService.deleteByPlayerId(id);
+                 return ResponseEntity.ok().build();
+             } else{
+                 throw new PlayerNotFoundException("Player with id -> " + id + " does not exist!!!");
+             }
+
+         } catch (Exception e){
+             System.out.println("Error ==>> " + e.getMessage());
          }
+         return null;
     }
 
 }
--- src/main/java/com/itacademy/dicesgame/service/impl/GameServiceImpl.java
+++ src/main/java/com/itacademy/dicesgame/service/impl/GameServiceImpl.java
@@ -2,6 +2,7 @@ package com.itacademy.dicesgame.service.impl;
 
 import com.itacademy.dicesgame.entity.Game;
 import com.itacademy.dicesgame.entity.Player;
+import com.itacademy.dicesgame.exception.PlayerNotFoundException;
 import com.itacademy.dicesgame.repository.IGameRepository;
 import com.itacademy.dicesgame.repository.IPlayerRepository;
 import com.itacademy.dicesgame.service.IGameService;
@@ -21,9 +22,18 @@ public class GameServiceImpl implements IGameService {
 
     @Override
     public Game rollDices(Long player_id) {
-        Game game = new Game(playerRepository.findById(player_id));
-        game.rollDices();
-        return gameRepository.save(game);
+        if(playerRepository.findById(player_id) != null){
+            try {
+                Game game = new Game(playerRepository.findById(player_id));
+                game.rollDices();
+                return gameRepository.save(game);
+            } catch (Exception e){
+                System.out.println("Error -> " + e.getMessage());
+            }
+        } else{
+            throw new PlayerNotFoundException("Player with id: " + player_id + " does not exist!!!");
+        }
+        return null;
     }
 
     @Override
